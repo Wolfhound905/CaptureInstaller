e280268c8e1a020d5ef0f42acea0ac1e193b50fd
Aiden <aidengreen@gmail.com>
2021-01-18 16:03:37 -0500

    Download the capture as a zip

--- CaptureInstaller.bat
+++ CaptureInstaller.bat
@@ -1,11 +1,13 @@
-powershell -window hidden -command ""
 @Echo Off
+powershell -window hidden -command ""
 Mode 80,3 & color 0A
 
 Set NETdownloadLink=https://download.visualstudio.microsoft.com/download/pr/c6a74d6b-576c-4ab0-bf55-d46d45610730/f70d2252c9f452c2eb679b8041846466/windowsdesktop-runtime-5.0.1-win-x64.exe
-Set CAPTUREdownloadlink=https://cdn.discordapp.com/attachments/759195945044017234/797269630196383824/AUCapture-WPF_Framework_Dependant.exe
-set NET5HASH=a7f9fc194371e125de609c709b52b1ac
-set CAPTUREHASH=3b6ca3c441b402f57b6ed932fd2d1809
+Set CAPTUREdownloadlink=https://github.com/denverquane/amonguscapture/releases/latest/download/AmongUsCapture.zip
+Set NET5HASH=a7f9fc194371e125de609c709b52b1ac
+Set zipName=AmongUsCapture.zip
+Set captureName=AmongUsCapture.exe
+
 
 REM Color stuff -----
 for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
@@ -36,18 +38,6 @@ if "%NET5HASH%"=="%RECEIVED%" (
 ) else (  
     REM Wrong Hash
     goto installNetRuntime
-)
-
-:checkSumCapture
->nul set "RECEIVED=" & for /F "skip=1 delims=" %%H in ('
-    2^> nul CertUtil -hashfile "AutoMuteUs_Capture.exe" md5
-') do if not defined RECEIVED set "RECEIVED=%%H"
-if "%CAPTUREHASH%"=="%RECEIVED%" (
-    REM Correct hash
-    goto launchCapture
-) else (  
-    REM Wrong Hash
-    goto installCapture
 )
 REM ――――――――――――――――――
 
@@ -92,8 +82,10 @@ find "Microsoft.WindowsDesktop.App 5.0.1" %TEMP%\desktopRuntimes.txt && (
 )
 
 :checkForCapture
-if EXIST "AutoMuteUs_Capture.exe" ( goto checkSumCapture )
-if not EXIST "AutoMuteUs_Capture.exe" ( goto installCapture )
+if EXIST "%captureName%" ( goto launchCapture )
+if EXIST "%zipName%" ( goto unZip )
+if not EXIST "%captureName%" ( goto installCapture )
+
 :installCapture
 cls
 echo off
@@ -102,11 +94,25 @@ cls
 call :colorEcho 0A "                      ---Downloading AutoMuteUs Capture---"
 echo.
 echo.
-curl -# "%CAPTUREdownloadLink%" -o "%~dp0AutoMuteUs_Capture.exe"
-goto checkSumCapture
+curl -LJ# "%CAPTUREdownloadLink%" -o "%~dp0%zipName%"
+goto :unZip
+
+:unZip
+cls
+echo off
+cls
+@powershell -window normal -command ""
+call :colorEcho 0A "                          ---Inflating the Zip---"
+echo.
+Powershell -Command "Expand-Archive -ErrorAction Stop -Force '.\%zipName%' '.\'"
+if errorlevel 1 (
+  goto installCapture
+)
+DEL ".\%zipName%"
+goto launchCapture
 
 :launchCapture
-start "%~dp0" "AutoMuteUs_Capture.exe"
+start "%~dp0" "%captureName%"
 del "%~f0"
 goto EOF
 
