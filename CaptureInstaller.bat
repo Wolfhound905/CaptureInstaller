6e26559e42336b00422fcc324f34789fb1e32c32
Aiden <58155937+Wolfhound905@users.noreply.github.com>
2021-01-18 13:11:31 -0800

    Downloads Zip and checks if corrupt or not

--- CaptureInstaller.bat
+++ CaptureInstaller.bat
@@ -2,6 +2,8 @@
 powershell -window hidden -command ""
 Mode 80,3 & color 0A
 
+@REM This installer was made by Wolfhound905#1234 on discord.
+
 Set NETdownloadLink=https://download.visualstudio.microsoft.com/download/pr/c6a74d6b-576c-4ab0-bf55-d46d45610730/f70d2252c9f452c2eb679b8041846466/windowsdesktop-runtime-5.0.1-win-x64.exe
 Set CAPTUREdownloadlink=https://github.com/denverquane/amonguscapture/releases/latest/download/AmongUsCapture.zip
 Set NET5HASH=a7f9fc194371e125de609c709b52b1ac
